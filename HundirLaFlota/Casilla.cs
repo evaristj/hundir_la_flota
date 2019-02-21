0ffc121b444281752fce393105743d40ee62df1e
evarist <evaristjuamet@gmail.com>
2019-02-21 12:50:38 +0100

    first commit, connect with github

---
+++ HundirLaFlota/Casilla.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HundirLaFlota
+{
+    class Casilla
+    {
+
+        private int file;
+        private int column;
+        private char water;
+        private char ship;
+        private char touched;
+
+        public Casilla(int file, int column, char water, char ship, char touched)
+        {
+            this.file = file;
+            this.column = column;
+            this.water = water;
+            this.ship = ship;
+            this.touched = touched;
+        }
+
+        public int ColumnTable
+        {
+            get
+            {
+               return column;
+            }
+            set
+            {
+                column = value;
+            }
+        }
+
+        public int FileTable { get => file; set => file = value; }
+        public char Water { get => water; set => water = value; }
+        public char Ship { get => ship; set => ship = value; }
+        public char Touched { get => touched; set => touched = value; }
+    }
+}
---
+++ HundirLaFlota/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HundirLaFlota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
